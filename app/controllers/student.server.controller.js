ab1029ea0341576e1afdf7abd0f7e60eb851ba78
Kenneth General <kennethlorenzg@gmail.com>
2021-03-13 21:40:53 -0500

    Update student.server.controller.js

--- app/controllers/student.server.controller.js
+++ app/controllers/student.server.controller.js
@@ -39,7 +39,7 @@ const getErrorMessage = function (err) {
 exports.create = function (req, res, next) {
   // Create a new instance of the 'Student' Mongoose model
   var student = new Student(req.body); //get data from React form
-  console.log("body: " + req.body.username);
+  console.log("body: " + req.body.studentNumber);
 
   // Use the 'Student' instance's 'save' method to save a new Student document
   student.save(function (err) {
@@ -51,4 +51,168 @@ exports.create = function (req, res, next) {
       res.json(student);
     }
   });
+};
+
+exports.list = function (req, res, next) {
+  // Use the 'Student' instance's 'find' method to retrieve a new student document
+  Student.find({}, function (err, students) {
+    if (err) {
+      return next(err);
+    } else {
+      res.json(students);
+    }
+  });
+};
+
+// authenticates a student
+exports.authenticate = function (req, res, next) {
+  // Get credentials from request
+  console.log(req.body);
+  const studentNumber = req.body.auth.studentNumber;
+  const password = req.body.auth.password;
+  console.log(password);
+  console.log(studentNumber);
+  //find the student with given studentNumber using static method findOne
+  Student.findOne({ studentNumber: studentNumber }, (err, student) => {
+    if (err) {
+      return next(err);
+    } else {
+      console.log(student);
+      //compare passwords
+      if (bcrypt.compareSync(password, student.password)) {
+        // Create a new token with the student id in the payload
+        // and which expires 300 seconds after issue
+        const token = jwt.sign(
+          { id: student._id, studentNumber: student.studentNumber },
+          jwtKey,
+          { algorithm: "HS256", expiresIn: jwtExpirySeconds }
+        );
+        console.log("token:", token);
+        // set the cookie as the token string, with a similar max age as the token
+        // here, the max age is in milliseconds
+        res.cookie("token", token, {
+          maxAge: jwtExpirySeconds * 1000,
+          httpOnly: true,
+        });
+        res.status(200).send({ screen: student.studentNumber });
+        //
+        //res.json({status:"success", message: "student found!!!", data:{student:
+        //student, token:token}});
+
+        req.student = student;
+        //call the next middleware
+        next();
+      } else {
+        res.json({
+          status: "error",
+          message: "Invalid student number/password!!!",
+          data: null,
+        });
+      }
+    }
+  });
+};
+
+// protected page uses the JWT token
+exports.welcome = (req, res) => {
+  // We can obtain the session token from the requests cookies,
+  // which come with every request
+  const token = req.cookies.token;
+  console.log(token);
+  // if the cookie is not set, return an unauthorized error
+  if (!token) {
+    return res.status(401).end();
+  }
+
+  var payload;
+  try {
+    // Parse the JWT string and store the result in `payload`.
+    // Note that we are passing the key in this method as well. This method will throw an error
+    // if the token is invalid (if it has expired according to the expiry time we set on sign in),
+    // or if the signature does not match
+    payload = jwt.verify(token, jwtKey);
+  } catch (e) {
+    if (e instanceof jwt.JsonWebTokenError) {
+      // if the error thrown is because the JWT is unauthorized, return a 401 error
+      return res.status(401).end();
+    }
+    // otherwise, return a bad request error
+    return res.status(400).end();
+  }
+
+  // Finally, return the welcome message to the student, along with their
+  // firstName given in the token
+  // use back-quotes here
+  res.send(`${payload.firstName}`);
+};
+
+//sign out function in controller
+//deletes the token on the client side by clearing the cookie named 'token'
+exports.signout = (req, res) => {
+  res.clearCookie("token");
+  return res.status("200").json({ message: "signed out" });
+  // Redirect the student back to the main application page
+  //res.redirect('/');
+};
+
+//check if the student is signed in
+exports.isSignedIn = (req, res) => {
+  // Obtain the session token from the requests cookies,
+  // which come with every request
+  const token = req.cookies.token;
+  console.log(token);
+  // if the cookie is not set, return 'auth'
+  if (!token) {
+    return res.send({ screen: "auth" }).end();
+  }
+  var payload;
+  try {
+    // Parse the JWT string and store the result in `payload`.
+    // Note that we are passing the key in this method as well. This method will throw an error
+    // if the token is invalid (if it has expired according to the expiry time we set on sign in),
+    // or if the signature does not match
+    payload = jwt.verify(token, jwtKey);
+  } catch (e) {
+    if (e instanceof jwt.JsonWebTokenError) {
+      // the JWT is unauthorized, return a 401 error
+      return res.status(401).end();
+    }
+    // otherwise, return a bad request error
+    return res.status(400).end();
+  }
+
+  // Finally, token is ok, return the firstName given in the token
+  res.status(200).send({ screen: payload.firstName });
+};
+
+//isAuthenticated() method to check whether a student is currently authenticated
+exports.requiresLogin = function (req, res, next) {
+  // Obtain the session token from the requests cookies,
+  // which come with every request
+  const token = req.cookies.token;
+  console.log(token);
+  // if the cookie is not set, return an unauthorized error
+  if (!token) {
+    return res.send({ screen: "auth" }).end();
+  }
+  var payload;
+  try {
+    // Parse the JWT string and store the result in `payload`.
+    // Note that we are passing the key in this method as well. This method will throw an error
+    // if the token is invalid (if it has expired according to the expiry time we set on sign in),
+    // or if the signature does not match
+    payload = jwt.verify(token, jwtKey);
+    console.log("in requiresLogin - payload:", payload);
+    req.id = payload.id;
+  } catch (e) {
+    if (e instanceof jwt.JsonWebTokenError) {
+      // if the error thrown is because the JWT is unauthorized, return a 401 error
+      return res.status(401).end();
+    }
+    // otherwise, return a bad request error
+    return res.status(400).end();
+  }
+  // student is authenticated
+  //call next function in line
+  next();
 };
