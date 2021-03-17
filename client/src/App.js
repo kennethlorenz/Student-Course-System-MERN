e8ce86b43adc1122f82c08b1657305a80b468cc4
Kenneth General <kennethlorenzg@gmail.com>
2021-03-16 21:18:40 -0400

    Update signin, signup page
    added proxy, added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -7,22 +7,27 @@ import {
   Redirect,
 } from "react-router-dom";
 //
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+//
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 
 function App() {
   return (
     <Router>
-      <Link to="/signin">Sign In</Link>
-      <Link to="/signup">Signup</Link>
-      <Switch>
-        <Route path="/signin">
-          <SignIn />
-        </Route>
-        <Route path="/signup">
-          <SignUp />
-        </Route>
-      </Switch>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">Student Course System with MERN stack</Nav>
+          <Nav.Link href="/signin">Sign In</Nav.Link>
+          <Nav.Link href="/signup">Sign Up</Nav.Link>
+        </Navbar.Collapse>
+      </Navbar>
+      <div>
+        <Route render={() => <SignIn />} path="/signin" />
+        <Route render={() => <SignUp />} path="/signup" />
+      </div>
     </Router>
   );
 }
